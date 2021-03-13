8dff06e4a82c99307f23d57c86ddca2b01757fe7
Yigong Qin <ygqin@c227-108.maverick2.tacc.utexas.edu>
2021-03-13 16:13:54 -0600

    generic scan

---
+++ hw2/codes/main.cpp
@@ -0,0 +1,271 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <string>
+#include <cstring>
+#include <omp.h>
+#include "CycleTimer.h"
+#include <mpi.h>
+// #include "ompscan.h"
+#define NUM_THREADS 2
+double cpu_plus_const(int N, int dim, float* sum0, float* resultarray);
+double cudaSum(int N, int dim, float* sum0, float* resultarray);
+double cudaScan(float* start, int N, int dim);
+double ompScan(float* start, int N, int dim);
+void printCudaInfo();
+
+static inline int nextPow2(int n)
+{
+    n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    n++;
+    return n;
+}
+
+void usage(const char* progname) {
+    printf("Usage: %s [options]\n", progname);
+    printf("Program Options:\n");
+    printf("  -m  --test <TYPE>      Run specified function on input.  Valid tests are: cuda, omp_cpu\n"); 
+    printf("  -i  --input <NAME>     Run test on given input type. Valid inputs  are: test1, random\n");
+    printf("  -n  --arraysize <INT>  length of array in one dimension\n");
+    printf("  -d  --arraydim <INT>   Dimensions of arrays\n");
+    printf("  -?  --help             This message\n");
+}
+
+
+// sequential code
+void cpu_inclusive_scan(float* start, int rank, int N, int dim)
+{
+    float*output = new float[N];
+    memmove(output, start, N*sizeof(float)); 
+    int length = N/dim;
+    for (int j = 0; j < dim; j++) {output[j] = start[j] ;}
+    for (int i = 1; i < length; i++)
+    {
+        for (int j = 0; j < dim; j++)
+           output[i*dim+j] = output[ (i-1)*dim +j] + start[ i*dim +j];
+    }
+   //printf("%d uuuu",N*rank);
+   for (int i = 0; i < N; i++) output[i] += N*rank;
+   memmove(start, output, N*sizeof(float));
+   delete[] output;
+}
+
+void initialization(std::string input, int N,  float* inarray, float* checkarray){
+
+    if (input.compare("random") == 0) {
+
+        srand(time(NULL));
+        //srand(1);
+        // generate random array
+        for (int i = 0; i < N; i++) {
+            float val = 1.0* rand() / RAND_MAX;
+            inarray[i] = val;
+            checkarray[i] = val;
+        }
+    } else if (input.compare("test1") == 0){
+        // default array of ones for debugging
+        for(int i = 0; i < N; i++){ 
+            inarray[i] = 1.0;
+            checkarray[i] = 1.0;}
+    }else
+      {for(int i = 0; i < N; i++) {inarray[i] = 1.0;}
+    }
+
+}
+
+int main(int argc, char** argv)
+{
+    int rank, size;
+    MPI_Comm comm = MPI_COMM_WORLD;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &size);
+    int N = 64;
+    int dim =1;
+    std::string test; 
+    std::string input;
+    int itrs = 1;
+    // parse commandline options ////////////////////////////////////////////
+    int opt;
+    static struct option long_options[] = {
+        {"test",       1, 0, 'm'},
+        {"arraysize",  1, 0, 'n'},
+        {"input",      1, 0, 'i'},
+        {"help",       0, 0, '?'},
+        {"dimension",     1, 0, 'd'},
+        {0 ,0, 0, 0}
+    };
+
+    while ((opt = getopt_long(argc, argv, "m:n:i:d:?t", long_options, NULL)) != EOF) {
+        switch (opt) {
+        case 'm':
+            test = optarg; 
+            break;
+        case 'n':
+            N = atoi(optarg);
+            break;
+        case 'i':
+            input = optarg;
+            break;
+        case 'd':
+            dim = atoi(optarg);
+            break;
+        case '?':
+        default:
+            usage(argv[0]);
+            return 1;
+        }
+    }
+    // end parsing of commandline options //////////////////////////////////////
+    if (rank==0)
+       printf("the length, dimensions and total size of the array: %d, %d, %d \n\n", N, dim, N*dim);
+    int Nall = N*dim;
+    N= (Nall+size-1)/size; // total size of the array;
+    if (rank==0) printf("each proccesor has size %d\n", N); 
+    float* inarray = new float[N];
+    float* checkarray = new float[N];
+    float* sum0 = new float[dim]; 
+    float* recvbuf = new float[dim];
+
+    double cudaTime=0,ompTime=0;
+
+    if (test.compare("cuda") == 0) { 
+        // run CUDA implementation
+        //printCudaInfo();
+        for (int i=0; i<itrs; i++) {
+                initialization(input, N, inarray, checkarray);
+                cudaTime += cudaScan(inarray, N, dim);
+        }
+        cudaTime/=itrs;
+        if (size==1) printf("GPU_time: %.3f ms\n", 1000.f * cudaTime);
+    } else if (test.compare("omp_cpu") == 0) { // Test find_repeats
+        printf("rank %d, uses number of threads: %d\n", rank, NUM_THREADS); 
+        for (int i=0; i<itrs; i++) {
+                initialization(input, N, inarray, checkarray);
+                ompTime += ompScan(inarray, N, dim);
+        } 
+        ompTime/=itrs;
+        if (size==1) printf("CPU_time: %.3f ms\n", 1000.f * ompTime);
+    } else { 
+        usage(argv[0]); 
+        exit(1); 
+    }
+    if (size >1){
+
+     // printf("MPI communication ----------------------------------------\n");
+      double startTime = CycleTimer::currentSeconds();
+      {for (int i = 0; i<dim; i++) {sum0[i] = inarray[i+N-dim];}}
+    // communication between cpus of the last element. 
+      MPI_Barrier( comm ); //printf("rank %d ready to communicate\n",rank);
+
+      if (rank !=0){ MPI_Recv(recvbuf, dim, MPI_FLOAT, rank-1 , rank-1, comm, MPI_STATUS_IGNORE);
+                  printf("rank %d recv message, tag %d \n",rank,rank-1);
+                  for (int i = 0; i<dim; i++) {sum0[i] += recvbuf[i];}}
+      if (rank!=size-1) {MPI_Send(sum0, dim, MPI_FLOAT, rank+1 , rank, comm);
+                  printf("rank %d send message, tag %d \n",rank,rank);}
+    double endTime = CycleTimer::currentSeconds(); 
+    printf("MPI communication time  %.3f ms\n", 1000.f *(endTime-startTime));
+     // printf("MPI communication ----------------------------------------\n");
+    //printf("rank %d, the summation from previous rank %f \n", rank, recvbuf[0]);
+    // add sum0 to every processor
+    if (test.compare("cuda") == 0) {
+        if (rank !=0)  cudaTime += cudaSum(N, dim, recvbuf, inarray); 
+       printf("GPU_time: %.3f ms\n", 1000.f * cudaTime); }
+    else if (test.compare("omp_cpu") == 0) { // Test find_repeats
+         if (rank !=0) ompTime += cpu_plus_const(N, dim, recvbuf, inarray);
+        printf("CPU_time: %.3f ms\n", 1000.f * ompTime);  
+    } else { usage(argv[0]);exit(1);}
+    }
+
+    if (((input.compare("test1") == 0) | (input.compare("test1") == 0) )){
+     // check with sequential code.
+     cpu_inclusive_scan(checkarray, rank, N, dim);
+    // printf("rank %d result \n",rank);for (int i = 0; i < 10; i++){printf("%f ",inarray[i]);}
+    // printf("\n");
+    // printf("rank %d check \n",rank);for (int i = 0; i < 10; i++){printf("%f ",checkarray[i]);}
+    // printf("\n");
+     for (int i = 0; i < N; i++)
+        {   //printf("real value = %d\n",checkarray[i]);
+            if( abs(checkarray[i] - inarray[i])/abs(checkarray[i])>1e-6 )
+            {
+                fprintf(stderr,
+                        "Error: Device inclusive_scan outputs incorrect result."
+                        " A[%d] = %f, expecting %f.\n",
+                        i, inarray[i], checkarray[i]);
+                exit(1);
+            }
+        }
+     printf("Scan outputs are correct!\n");
+    }
+
+    delete[] inarray;
+    delete[] checkarray;
+    return 0;
+}
+
+double ompScan( float* start, int length, int dim ){
+
+  int lens = length/dim;
+  int round_lens = nextPow2(lens);
+  int N=dim*round_lens;
+  float* output = new float[N];  
+  memmove(output, start, length*sizeof(float)); 
+  omp_set_num_threads(NUM_THREADS);
+  double startTime = CycleTimer::currentSeconds();
+ 
+    // upsweep phase  
+    for (int twod = 1; twod < round_lens; twod*=2){
+        int twod1 = twod*2;
+        int intvl = dim*twod1;
+        #pragma omp parallel for
+        for (int i = 0; i < N; i += intvl){ 
+           for (int j = 0; j < dim; j++){
+             output[i+(twod1-1)*dim +j] += output[i+(twod-1)*dim +j];}}
+       // implicit synchronization after omp parallel for 
+    }
+
+    // downsweep phase
+    for (int twod = round_lens/4; twod >= 1; twod /= 2){
+        int twod1 = twod*2;
+        int intvl = dim*twod1;
+        #pragma omp parallel for
+        for (int i=intvl; i < N; i += intvl){
+          for (int j = 0; j < dim; j++){
+             output[i+(twod-1)*dim +j] += output[i-1*dim +j];}}
+    }
+    #pragma omp barrier
+   
+    double endTime = CycleTimer::currentSeconds();
+    double overallDuration = endTime - startTime;
+
+   memmove(start, output, length*sizeof(float));
+   delete[] output;
+   return overallDuration;
+}
+
+double cpu_plus_const(int N, int dim, float* sum0, float* resultarray){
+
+  int dim_id;
+  double startTime = CycleTimer::currentSeconds();
+
+     #pragma omp for
+     for (int i=0; i<N; i++){
+          dim_id = i%dim;
+          resultarray[i] += sum0[dim_id]; }
+
+  double endTime = CycleTimer::currentSeconds();
+  double overallDuration = endTime - startTime;
+   return overallDuration;
+}
+
+
+
+
+
+
+
---
+++ hw2/codes/scan.cu
@@ -0,0 +1,173 @@
+#include <stdio.h>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <driver_functions.h>
+
+
+#include "CycleTimer.h"
+
+
+extern float toBW(int bytes, float sec);
+
+
+/* Helper function to round up to a power of 2. 
+ */
+static inline int nextPow2(int n)
+{
+    n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    n++;
+    return n;
+}
+
+// Device code
+
+__global__ void
+upsweep(int N, int dim, int twod, int twod1, float* output) {
+
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (index < N && ( (index/dim) % twod1 ==0) )
+       {output[index+ dim*(twod1 -1)] += output[index+ dim*(twod -1)];}
+}
+
+__global__ void
+downsweep(int N, int dim, int twod, int twod1, float* output) {
+
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if ( (0< index) && (index < N) && ( (index/dim) % twod1 ==0) ){
+         output[index+ dim*(twod-1)] += output[index- dim*1];}
+}
+
+__global__ void
+sum_const(int N, int dim, float* const_array, float* output){
+
+    // here N is the total length n*dim
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+    int dim_id = index%dim; 
+
+    if ( index < N ){
+         output[index] += const_array[dim_id];}
+
+}
+
+
+
+void inclusive_scan(int N, int lens, int dim, float* device_result){
+
+    int blocksize = 512;
+    int num_blocks = (N+blocksize-1)/blocksize;
+    printf("block size = %d, number of blocks %d \n",blocksize,num_blocks); 
+    for (int twod =1; twod <lens; twod *=2){
+        int twod1 = twod*2;
+            upsweep<<< num_blocks, blocksize  >>>(N, dim, twod, twod1, device_result);        
+    }
+
+    for (int twod = lens/4; twod >=1; twod /=2){
+        int twod1 = twod*2;
+            downsweep<<< num_blocks, blocksize  >>>(N, dim, twod, twod1, device_result);
+    }
+}
+
+void plus_const(int N, int dim, float* const_array, float* device_result){
+    // here N is the total length n*dim
+    int blocksize = 256;
+    int num_blocks = (N+blocksize-1)/blocksize;
+    sum_const<<< num_blocks, blocksize  >>>(N, dim, const_array, device_result);
+
+}
+
+
+// cuda scan
+double cudaScan(float* inarray, int N, int dim)
+{
+    // N is the total length of the array N = n*dim;
+    // round N to the next power of 2 to call recursive kernels;
+    int lens = N/dim; //printf("lens %d",lens); 
+    int round_lens = nextPow2(lens);
+    int rounded_length=dim*round_lens; //printf("the rounded length %d,blocks %d",rounded_length,rounded_length/512);
+    float* device_result;
+
+    cudaMalloc((void **)&device_result, sizeof(float) * rounded_length);
+
+    cudaMemcpy(device_result, inarray, N * sizeof(float), 
+               cudaMemcpyHostToDevice);
+
+    double startTime = CycleTimer::currentSeconds();
+
+    inclusive_scan(rounded_length, round_lens, dim, device_result);
+
+    // Wait for any work left over to be completed.
+    cudaDeviceSynchronize();
+    double endTime = CycleTimer::currentSeconds();
+    double overallDuration = endTime - startTime;
+    
+    cudaMemcpy(inarray, device_result, N * sizeof(float),
+               cudaMemcpyDeviceToHost);
+
+    cudaFree(device_result);
+    return overallDuration;
+}
+
+// cuda parallel sum
+double cudaSum(int N, int dim, float* sum0, float* resultarray)
+{   // here N is the total length n*dim
+    float* device_result;
+    float* device_sum;
+    cudaMalloc((void **)&device_result, sizeof(float) * N);
+    cudaMalloc((void **)&device_sum, sizeof(float) * dim)
+;
+    cudaMemcpy(device_result, resultarray, N * sizeof(float),
+               cudaMemcpyHostToDevice);
+    cudaMemcpy(device_sum, sum0, dim * sizeof(float),
+               cudaMemcpyHostToDevice);
+
+    double startTime = CycleTimer::currentSeconds();
+    //printf("the length %d", rounded_length);
+    plus_const(N, dim, device_sum,  device_result);//exclusive_scan(device_input, end - inarray, device_result);
+
+    // Wait for any work left over to be completed.
+    cudaDeviceSynchronize();
+    double endTime = CycleTimer::currentSeconds();
+    double overallDuration = endTime - startTime;
+
+    cudaMemcpy(resultarray, device_result, N * sizeof(float),
+               cudaMemcpyDeviceToHost);
+    cudaFree(device_result);
+    cudaFree(device_sum);
+    //for (int i = 0; i < rounded_length; i++){printf("%d ",resultarray[i]);}
+    return overallDuration;
+}
+
+
+
+
+void printCudaInfo()
+{
+    // for fun, just print out some stats on the machine
+
+    int deviceCount = 0;
+    cudaError_t err = cudaGetDeviceCount(&deviceCount);
+
+    printf("---------------------------------------------------------\n");
+    printf("Found %d CUDA devices\n", deviceCount);
+
+    for (int i=0; i<deviceCount; i++)
+    {
+        cudaDeviceProp deviceProps;
+        cudaGetDeviceProperties(&deviceProps, i);
+        printf("Device %d: %s\n", i, deviceProps.name);
+        printf("   SMs:        %d\n", deviceProps.multiProcessorCount);
+        printf("   Global mem: %.0f MB\n",
+               static_cast<float>(deviceProps.totalGlobalMem) / (1024 * 1024));
+        printf("   CUDA Cap:   %d.%d\n", deviceProps.major, deviceProps.minor);
+    }
+    printf("---------------------------------------------------------\n"); 
+}
